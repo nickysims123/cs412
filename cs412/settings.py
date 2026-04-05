4c6d04248f8f8f52fe72fb4f1fbe5305ef6bab74
Nicholas Sima <nick.sima@outlook.com>
2026-04-05 17:40:31 -0400

    pushing .settings REST_FRAMEWORK variable creation

--- cs412/settings.py
+++ cs412/settings.py
@@ -142,3 +142,10 @@ CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
     STATIC_URL = '/nicksima/static/'
     MEDIA_URL = '/nicksima/media/'
+
+
+REST_FRAMEWORK = {
+'DEFAULT_PAGINATION_CLASS':
+'rest_framework.pagination.PageNumberPagination',
+'PAGE_SIZE': 10
+}
